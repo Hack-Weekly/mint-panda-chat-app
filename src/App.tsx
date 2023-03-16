ca22c307468db97a5e10c3694973846a0dcc1593
Matt <matthew.defusco@gmail.com>
2023-03-16 11:12:14 -0400

    trying to test out adding a collection and document

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import './App.css';
 
+import { testCall } from './api/firebase';
+
 const App: React.FC = () => {
+  useEffect(() => {
+    testCall();
+  }, []);
   return (
     <div>
       <h1>Main App Page</h1>
