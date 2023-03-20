c067d018f2a5867250283e12e8ff81b279f07f9b
Aaron Wong <aaronwong52@gmail.com>
2023-03-20 15:30:17 -0400

    added styling for login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { auth, signInWithGoogle } from "../api/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
+import "./Login.css";
+
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -21,9 +23,16 @@ function Login() {
     }, [user, loading]);
 
     return (
-        <div >
-            <button onClick={signInWithGoogle}>
-                Login with Google
+        <div id="login-page">
+            <h1>Chat App</h1>
+            <form id="login-form">
+                <input id="nickname-input" type="text" placeholder="Nickname"></input>
+                <input id="sign-in-button" type="submit" value="Sign in"></input>
+            </form>
+            <p>or</p>
+            <button id="google-login-button" onClick={signInWithGoogle}>
+                <img src="/images/google.png" width="35" height="35" alt="google"></img>
+                Sign in with Google
             </button>
         </div>
     );
